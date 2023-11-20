7899f94a130557ac535a0ba79e3c22d38d978b18
Jhonathan <jhonathansin@hotmail.com>
2023-11-20 16:05:04 -0300

    Alteracoes no arquivo index para teste

--- ch04/public/index.php
+++ ch04/public/index.php
@@ -1,1 +1,18 @@
-<?php echo "I'm the new document root!"; ?>
+<?php
+declare(strict_types=1);
+
+/*
+ *  Include necessary files 
+ */
+include_once '../sys/core/init.inc.php';
+
+/*
+ *  Load the calendar for january 
+ */
+$cal = new Calendar($dbo, "2016,01,01 12:00:00");
+
+if (is_object($cal)) {
+    echo "<pre>" . var_dump($cal) . "</pre>";
+}
+
+?>
--- ch04/sys/core/init.inc.php
+++ ch04/sys/core/init.inc.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /*
  *  Include the necessary configuration info 
  */
-include_once '../sys/config/db-cred.inc.php';
+include_once '../config/db-cred.inc.php';
 
 /*
  *  Define constants for configuration info 
@@ -22,7 +22,7 @@ $dbo = new PDO($dsn, DB_USER, DB_PASS);
 /*
  *  Define the auto-load function for classes 
  */
-function __autoload($class) {
+function spl_autoload_register($class) {
     $filename = '..sys/class/class.' . $class . ".inc.php";
     if (file_exists($filename)) {
         include_once $filename;
